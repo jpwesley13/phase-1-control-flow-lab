382bd86097f1e28ad4c67cdfba2adf1f12aa5864
Jared Patterson <jpwesley34@gmail.com>
2023-10-30 17:12:06 -0700

    rides scooters

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let charge
+  if (distance < 400) {
+    charge = "This one is on me!";
+  }
+  else if (distance >= 400 && distance <= 2000) {
+    charge = "That will be twenty bucks."
+  }
+  else if (distance > 2000 && distance <= 2500) {
+    charge = "I will gladly take your thirty bucks.";
+  }
+  else {
+    charge = "No can do."
+  }
+  
+  return charge;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC"?"Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generosity){
   // Write your code here!
+  switch(generosity){
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:
+      return "Bye."
+  }
 }
